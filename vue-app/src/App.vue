57af89887f8f6cc8dd25cd0b3327acff2987ce42
Patricia Pieroni <patriciaamarante@hotmail.com>
2023-06-30 14:00:23 -0300

    app

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,6 +1,64 @@
 <template>
   <div class="container">
     <h1>Finance Manager</h1>
+    <div v-if="!editingTransaction && !addingTransaction">
+            <button class="btn btn-primary form-right" @click="addingTransaction = true">New Transaction</button>
+          </div>
+    <div class="section">
+      <div v-if="!editingTransaction && addingTransaction" class="form-container">
+        <h2>Add Transaction</h2>
+        <form @submit.prevent="addTransaction">
+          <label>
+            Type:
+            <select v-model="newTransaction.type" required>
+              <option value="income">Income</option>
+              <option value="expense">Expense</option>
+            </select>
+          </label>
+          <label>
+            Date:
+            <input type="date" v-model="newTransaction.date" required>
+          </label>
+          <label>
+            Description:
+            <input type="text" v-model="newTransaction.description" required>
+          </label>
+          <label>
+            Amount:
+            <input type="number" v-model="newTransaction.amount" required min="0" :class="{ 'error': !isPositiveNumber(newTransaction.amount) }">
+          </label>
+          <button class="btn btn-primary" type="submit">Add</button>
+          <button class="btn btn-delete" @click="cancelAdd">Cancel</button>
+        </form>
+      </div>
+
+      <div v-if="editingTransaction" class="form-container">
+        <h2>Edit Transaction</h2>
+        <form @submit.prevent="updateTransaction">
+          <label>
+            Type:
+            <select v-model="editingTransaction.type" required>
+              <option value="income">Income</option>
+              <option value="expense">Expense</option>
+            </select>
+          </label>
+          <label>
+            Date:
+            <input type="date" v-model="editingTransaction.date" required>
+          </label>
+          <label>
+            Description:
+            <input type="text" v-model="editingTransaction.description" required>
+          </label>
+          <label>
+            Amount:
+<input type="number" v-model="editingTransaction.amount" required min="0" :class="{ 'error': !isPositiveNumber(editingTransaction.amount) }">
+                   </label>
+          <button class="btn btn-primary" type="submit">Update</button>
+          <button class="btn btn-delete" @click="cancelEdit">Cancel</button>
+        </form>
+      </div>
+    </div>
 
     <h2>Transactions</h2>
     <table class="table" style="width: 100%;">
@@ -16,7 +74,7 @@
         <tr v-for="(transaction, index) in transactions" :key="index">
           <td>{{ new Date(transaction.date).toLocaleDateString('pt-BR') }}</td>
           <td>{{ transaction.description }}</td>
-          <td v-bind:class="{ 'negative': transaction.amount < 0, 'positive': transaction.amount > 0 }">
+          <td v-bind:class="{ 'negative': transaction.type === 'expense', 'positive': transaction.type === 'income' }">
             {{
               transaction.amount.toLocaleString('pt-BR', {
                 style: 'currency',
@@ -29,92 +87,67 @@
           </td>
         </tr>
       </tbody>
-
     </table>
-     <tfoot>
-            <tr>
-              <td>Balance:</td>
-              <td></td>
-              <td v-bind:class="{ 'negative': calculateBalance() < 0, 'positive': calculateBalance() > 0 }">
-                {{
-                  calculateBalance().toLocaleString('pt-BR', {
-                    style: 'currency',
-                    currency: 'BRL'
-                  })
-                }}</td>
-              <td></td>
-            </tr>
-          </tfoot>
-
-    <div v-if="!editingTransaction" class="form-container">
-      <h2>Add Transaction</h2>
-      <form @submit.prevent="addTransaction">
-        <label>
-          Date:
-          <input type="date" v-model="newTransaction.date" required>
-        </label>
-        <label>
-          Description:
-          <input type="text" v-model="newTransaction.description" required>
-        </label>
-        <label>
-          Amount:
-          <input type="number" v-model="newTransaction.amount" required>
-        </label>
-        <button class="btn btn-primary" type="submit">Add</button>
-      </form>
-    </div>
-
-    <div v-if="editingTransaction" class="form-container">
-      <h2>Edit Transaction</h2>
-      <form @submit.prevent="updateTransaction">
-        <label>
-          Date:
-          <input type="date" v-model="editingTransaction.date" required>
-        </label>
-        <label>
-          Description:
-          <input type="text" v-model="editingTransaction.description" required>
-        </label>
-        <label>
-          Amount:
-          <input type="number" v-model="editingTransaction.amount" required>
-        </label>
-        <button class="btn btn-primary" type="submit">Update</button>
-        <button class="btn btn-delete" @click="cancelEdit">Cancel</button>
-      </form>
-    </div>
-
-
+    <tfoot>
+      <tr>
+        <td>Balance:</td>
+        <td></td>
+        <td v-bind:class="{ 'negative': calculateBalance() < 0, 'positive': calculateBalance() > 0 }">
+          {{
+          calculateBalance().toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+          })
+          }}</td>
+        <td></td>
+      </tr>
+    </tfoot>
   </div>
 </template>
 <script>
 import { ref } from 'vue';
-import transactionsData from "./assets/data.json";
+import transactionsData from "./assets/data10000.json";
 
 
 export default {
   name: 'TransactionList',
   setup() {
-    
+
+    const addingTransaction = ref(false);
     const transactions = ref(transactionsData);
-    const newTransaction = ref({ id: 0, date: new Date(), description: '', amount: 0 });
+    const newTransaction = ref({ id: 0, date: new Date(), description: '', amount: 0, type: 'income' || 'expense' });
     const editingTransaction = ref(null);
+
 
     const addTransaction = () => {
       transactions.value.push({ ...newTransaction.value });
-      newTransaction.value = {id: 0, date: new Date(), description: '', amount: 0 };
+      cancelAdd();
     };
 
     const editTransaction = (transaction) => {
+        cancelAdd();
       editingTransaction.value = { ...transaction };
     };
+
+    const cancelEdit = () => {
+          editingTransaction.value = null;
+        };
+
+        const cancelAdd = () => {
+         newTransaction.value = {id: 0, date: new Date(), description: '', amount: 0,  type: 'income' || 'expense'  };
+          addingTransaction.value = false;
+        };
+
 
     const calculateBalance = () => {
       return transactions.value.reduce((total, transaction) => {
         return total + transaction.amount;
       }, 0);
     };
+
+     const isPositiveNumber = (amount) => {
+          return Number(amount) > 0;
+        };
 
 
     const updateTransaction = () => {
@@ -138,12 +171,16 @@ export default {
     return {
       transactions: transactionsData,
       newTransaction,
-      editingTransaction,
-      addTransaction,
-      editTransaction,
-      updateTransaction,
-      deleteTransaction,
-      calculateBalance,
+            editingTransaction,
+            addTransaction,
+            editTransaction,
+            updateTransaction,
+            deleteTransaction,
+            calculateBalance,
+            addingTransaction,
+            cancelEdit,
+            cancelAdd,
+            isPositiveNumber,
     };
   },
 };
@@ -317,11 +354,15 @@ input[type='date'] {
 }
 
 .form-group input[name="description"] {
-  width: 400px;
+  width: 420px;
 }
 
 .form-group input[name="amount"] {
   width: 150px;
+}
+
+.form-right {
+  float: right;
 }
 
 
